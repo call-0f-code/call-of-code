beb258b578578744ada8c560327a453e92257126
shivaji <108468912+shivaji1134@users.noreply.github.com>
2024-10-21 23:23:43 +0530

    made final homePage changes

--- components/Home.tsx
+++ components/Home.tsx
@@ -20,14 +20,13 @@ export default function ParticlesDemo() {
 
   return (
     <div className="relative flex h-screen w-full flex-col items-center justify-center overflow-hidden bg-background">
-      <span className="pointer-events-none relative z-10 whitespace-pre-wrap bg-gradient-to-b from-foreground to-foreground/60 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent">
-        &lt;&gt; CALL OF CODE &lt;/&gt;
-      </span>
+      <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-r from-red-600 via-purple-600 to-blue-500 dark:from-red-600 dark:via-purple-600 dark:to-blue-500 text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-sans relative z-20 font-bold tracking-tight leading-tight"> &lt;&gt; CALL OF CODE &lt;/&gt; </h2> <p className="max-w-xl mx-auto text-sm sm:text-base md:text-lg lg:text-xl text-neutral-700 dark:text-neutral-400 text-center leading-relaxed"> Welcome to Call of Code! Join us to explore, innovate, and collaborate on exciting projects while enhancing your coding skills through workshops, hackathons, and peer learning. </p>
+      
       <Particles
         className="absolute inset-0"
         quantity={150}
         ease={80}
-        size={2.0}
+        size={1.25}
         color={color}
         refresh
       />
