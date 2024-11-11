e500d1214c02e9645de78fc0643b17a43a83aaf4
shivaji <108468912+shivaji1134@users.noreply.github.com>
2024-11-11 11:19:52 +0530

    changed navbar to hamburger\

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -46,7 +46,7 @@ export function FloatingNavbar() {
   ]
 
   return (
-    <div className="fixed bottom-4 left-4 z-50 md:bottom-0 md:left-0 md:w-full md:flex md:justify-center md:pb-4">
+    <div className="fixed bottom-4 left-4 z-50  md:bottom-0 md:left-0 md:w-full md:flex md:justify-center md:pb-4">
       <FloatingDock 
         mobileClassName="flex-row space-x-2 origin-left"
         items={links}
